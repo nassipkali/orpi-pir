68c18bf00f26b3b7253ac79eba6c63caa8d4b81a
Daulet Nassipkali <uralskdev@gmail.com>
2022-08-27 22:45:41 +0500

    Update main.c

--- main.c
+++ main.c
@@ -15,15 +15,15 @@ int main (void) {
         printf("val = %d\n", val);
         sleep(1);
         if(val == HIGH) { 
-            if(pirState == LOW) {
-                printf("Motion detected");
-                system("curl -X POST -d '{ \"key\": \"value\" }' http://192.168.1.149:8123/api/webhook/testwebhook-thEeT-SJdULqCSeidQLJDSeG");
-			    sleep(10);
-                pirState = HIGH;
+			if(pirState == LOW) {
+				printf("Motion detected");
+				system("curl -X POST -d '{ \"key\": \"value\" }' http://192.168.1.149:8123/api/webhook/testwebhook-thEeT-SJdULqCSeidQLJDSeG");
+				sleep(10);
+				pirState = HIGH;
             }
         }
         else {
-            if(pirState == HIGH) {
+			if(pirState == HIGH) {
 			    printf("Motion ended");
 			    pirState = LOW;
 		    }
